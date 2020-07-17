980c52729b7bbf3fef8d55506e8545cb012f39f8
Bill <yuanweibiao@pdnews.com>
2020-07-17 13:33:18 +0800

    .

--- app/src/main/java/com/bill/umengtest/ShareActivity.java
+++ app/src/main/java/com/bill/umengtest/ShareActivity.java
@@ -1,9 +1,10 @@
 package com.bill.umengtest;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.umeng.socialize.ShareAction;
 import com.umeng.socialize.UMShareAPI;
